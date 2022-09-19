webwarrior-ws/DotNetLightning

commit 6b3095e11fe7822634f7683fa43780f0fbd36e02

Core: support variable payload length on fillerGeneration
Original implementation from
https://github.com/ElementsProject/lightning/blob/ade10e7fc4dacbb9d635b05152c7dc38c0896ce7/contrib/pyln-proto/pyln/proto/onion.py#L497

src/DotNetLightning.Core/Crypto/Sphinx.fs
@@ -29,6 +29,9 @@ module Sphinx =
     let PayloadLength = 33
 
     [<Literal>]
+    let HopDataSize = 1300
+
+    [<Literal>]
     let MacLength = 32
 
     [<Literal>]
@@ -123,26 +126,41 @@ module Sphinx =
 
     let rec internal generateFiller
         (keyType: string)
+        (payloads: list<array<byte>>)
         (sharedSecrets: list<Key>)
-        (hopSize: int)
-        (maxNumberOfHops: option<int>)
         =
-        let maxHopN = defaultArg maxNumberOfHops MaxHops
+        let fillerSize =
+            payloads.[1..]
+            |> List.sumBy(fun payload -> payload.Length + MacLength)
 
-        sharedSecrets
-        |> List.fold
-            (fun (padding: array<byte>) (secret: Key) ->
-                let key = generateKey(keyType, secret.ToBytes())
-                let padding1 = Array.append padding (zeros hopSize)
+        let rec fillInner (filler: array<byte>) (i: int) : array<byte> =
+            if i = payloads.Length - 1 then
+                filler
+            else
+                let fillerOffset =
+                    payloads.[.. i - 1]
+                    |> List.sumBy(fun payload -> payload.Length + MacLength)
+
+                let fillerStart = HopDataSize - fillerOffset
+                let fillerEnd = HopDataSize + payloads.[i].Length + MacLength
+                let fillerLength = fillerEnd - fillerStart
+
+                let key = generateKey(keyType, sharedSecrets.[i].ToBytes())
 
                 let stream =
-                    let s = generateStream(key, hopSize * (maxHopN + 1))
-                    s.[s.Length - padding1.Length .. s.Length - 1] // take padding1 from tale
+                    let s = generateStream(key, fillerEnd)
+                    s.[fillerStart .. fillerEnd - 1]
 
-                assert (stream.Length = padding1.Length)
-                xor(padding1, stream)
-            )
-            [||]
+                let newFiller =
+                    [
+                        xor(Array.take fillerLength filler, stream)
+                        Array.skip fillerLength filler
+                    ]
+                    |> Array.concat
+
+                fillInner newFiller (i + 1)
+
+        fillInner (Array.zeroCreate fillerSize) 0
 
     type ParsedPacket =
         {
@@ -300,12 +318,7 @@ module Sphinx =
                     (sessionKey)
                     (pubKeys)
 
-            let filler =
-                generateFiller
-                    "rho"
-                    sharedSecrets.[0 .. sharedSecrets.Length - 2]
-                    (PayloadLength + MacLength)
-                    (Some MaxHops)
+            let filler = generateFiller "rho" payloads sharedSecrets
 
             let lastPacket =
                 makeNextPacket(

tests/DotNetLightning.Core.Tests/SphinxTests.fs
@@ -105,12 +105,7 @@ let bolt4Tests1 =
                         sessionKey
                         pubKeys
 
-                let filler =
-                    generateFiller
-                        "rho"
-                        sharedSecrets.[0 .. sharedSecrets.Length - 2]
-                        (PayloadLength + MacLength)
-                        (Some(20))
+                let filler = generateFiller "rho" payloads sharedSecrets
 
                 let expectedFiller =
                     "c6b008cf6414ed6e4c42c291eb505e9f22f5fe7d0ecdd15a833f4d016ac974d33adc6ea3293e20859e87ebfb937ba406abd025d14af692b12e9c9c2adbe307a679779259676211c071e614fdb386d1ff02db223a5b2fae03df68d321c7b29f7c7240edd3fa1b7cb6903f89dc01abf41b2eb0b49b6b8d73bb0774b58204c0d0e96d3cce45ad75406be0bc009e327b3e712a4bd178609c00b41da2daf8a4b0e1319f07a492ab4efb056f0f599f75e6dc7e0d10ce1cf59088ab6e873de377343880f7a24f0e36731a0b72092f8d5bc8cd346762e93b2bf203d00264e4bc136fc142de8f7b69154deb05854ea88e2d7506222c95ba1aab065c8a851391377d3406a35a9af3ac"

commit 6437718eca57e748bd4802f8a8a7e3d2d56ab20e

Sphinx: fix generateFiller function
This problem was introduced in
https://github.com/nblockchain/DotNetLightning.Kiss/commit/6b3095e11fe7822634f7683fa43780f0fbd36e02.

New implementation is ported from
https://github.com/ACINQ/eclair/blob/9d17b1d/eclair-core/src/main/scala/fr/acinq/eclair/crypto/Sphinx.scala.

src/DotNetLightning.Core/Crypto/Sphinx.fs
@@ -125,44 +125,6 @@ module Sphinx =
             ([ blindingFactor0 ])
             ([ secret0 ])
 
-    let rec internal generateFiller
-        (keyType: string)
-        (payloads: list<array<byte>>)
-        (sharedSecrets: list<Key>)
-        =
-        let fillerSize =
-            payloads.[1..]
-            |> List.sumBy(fun payload -> payload.Length + MacLength)
-
-        let rec fillInner (filler: array<byte>) (i: int) : array<byte> =
-            if i = payloads.Length - 1 then
-                filler
-            else
-                let fillerOffset =
-                    payloads.[.. i - 1]
-                    |> List.sumBy(fun payload -> payload.Length + MacLength)
-
-                let fillerStart = HopDataSize - fillerOffset
-                let fillerEnd = HopDataSize + payloads.[i].Length + MacLength
-                let fillerLength = fillerEnd - fillerStart
-
-                let key = generateKey(keyType, sharedSecrets.[i].ToBytes())
-
-                let stream =
-                    let s = generateStream(key, fillerEnd)
-                    s.[fillerStart .. fillerEnd - 1]
-
-                let newFiller =
-                    [
-                        xor(Array.take fillerLength filler, stream)
-                        Array.skip fillerLength filler
-                    ]
-                    |> Array.concat
-
-                fillInner newFiller (i + 1)
-
-        fillInner (Array.zeroCreate fillerSize) 0
-
     let peekPayloadLength(bytes: array<byte>) =
         result {
             match bytes |> Array.head with
@@ -171,6 +133,38 @@ module Sphinx =
                 let! length, _ = bytes.TryPopVarInt()
                 return int length + MacLength + length.ToVarInt().Length
         }
+
+
+    let rec internal generateFiller
+        (keyType: string)
+        (payloads: list<array<byte>>)
+        (sharedSecrets: list<Key>)
+        =
+        List.zip payloads sharedSecrets
+        |> List.fold
+            (fun (padding: array<byte>) (payload, sharedSecret) ->
+                match peekPayloadLength payload with
+                | Ok perHopPayloadLength ->
+                    let key = generateKey(keyType, sharedSecret.ToBytes())
+
+                    let padding1 =
+                        Array.append
+                            padding
+                            (Array.zeroCreate perHopPayloadLength)
+
+                    let stream =
+                        generateStream(key, HopDataSize + perHopPayloadLength)
+                        |> Array.skip(
+                            HopDataSize + perHopPayloadLength - padding1.Length
+                        )
+
+                    xor(padding1, stream)
+                | Error err ->
+                    failwithf
+                        "generateFiller::peekPayloadLength returned an error: %s"
+                        err
+            )
+            Array.empty
 
     type ParsedPacket =
         {
@@ -348,7 +342,11 @@ module Sphinx =
                     (sessionKey)
                     (pubKeys)
 
-            let filler = generateFiller "rho" payloads sharedSecrets
+            let filler =
+                generateFiller
+                    "rho"
+                    (payloads |> List.take(payloads.Length - 1))
+                    (sharedSecrets |> List.take(sharedSecrets.Length - 1))
 
             let initialPacket =
                 { OnionPacket.LastPacket with

tests/DotNetLightning.Core.Tests/SphinxTests.fs
@@ -105,7 +105,11 @@ let bolt4Tests1 =
                         sessionKey
                         pubKeys
 
-                let filler = generateFiller "rho" payloads sharedSecrets
+                let filler =
+                    generateFiller
+                        "rho"
+                        (payloads |> List.take(payloads.Length - 1))
+                        (sharedSecrets |> List.take(sharedSecrets.Length - 1))
 
                 let expectedFiller =
                     "b77d99c935d3f32469844f7e09340a91ded147557bdd0456c369f7e449587c0f5666faab58040146db49024db88553729bce12b860391c29c1779f022ae48a9cb314ca35d73fc91addc92632bcf7ba6fd9f38e6fd30fabcedbd5407b6648073c38331ee7ab0332f41f550c180e1601f8c25809ed75b3a1e78635a2ef1b828e92c9658e76e49f995d72cf9781eec0c838901d0bdde3ac21c13b4979ac9e738a1c4d0b9741d58e777ad1aed01263ad1390d36a18a6b92f4f799dcf75edbb43b7515e8d72cb4f827a9af0e7b9338d07b1a24e0305b5535f5b851b1144bad6238b9d9482b5ba6413f1aafac3cdde5067966ed8b78f7c1c5f916a05f874d5f17a2b7d0ae75d66a5f1bb6ff932570dc5a0cf3ce04eb5d26bc55c2057af1f8326e20a7d6f0ae644f09d00fac80de60f20aceee85be41a074d3e1dda017db79d0070b99f54736396f206ee3777abd4c00a4bb95c871750409261e3b01e59a3793a9c20159aae4988c68397a1443be6370fd9614e46108291e615691729faea58537209fa668a172d066d0efff9bc77c2bd34bd77870ad79effd80140990e36731a0b72092f8d5bc8cd346762e93b2bf203d00264e4bc136fc142de8f7b69154deb05854ea88e2d7506222c95ba1aab065c8a"