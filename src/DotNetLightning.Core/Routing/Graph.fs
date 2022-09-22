8dde651de8128aa88a6c93313e8d39468addbce3
webwarrior <reg@webwarrior.ws>
2022-09-22 13:36:15 +0200

    Applied autoformatting to Graph.fs

--- src/DotNetLightning.Core/Routing/Graph.fs
+++ src/DotNetLightning.Core/Routing/Graph.fs
@@ -35,13 +35,14 @@ type IRoutingHopInfo =
     abstract HTLCMinimumMSat: LNMoney
 
 
-type RoutingGrpahEdge = 
+type RoutingGrpahEdge =
     {
-        Source : NodeId
-        Target : NodeId
-        ShortChannelId : ShortChannelId
+        Source: NodeId
+        Target: NodeId
+        ShortChannelId: ShortChannelId
         Update: UnsignedChannelUpdateMsg
     }
+
     interface IEdge<NodeId> with
         member this.Source = this.Source
         member this.Target = this.Target
@@ -50,8 +51,13 @@ type RoutingGrpahEdge =
         override self.NodeId = self.Source
         override self.ShortChannelId = self.Update.ShortChannelId
         override self.FeeBaseMSat = self.Update.FeeBaseMSat
-        override self.FeeProportionalMillionths = self.Update.FeeProportionalMillionths
-        override self.CltvExpiryDelta = self.Update.CLTVExpiryDelta.Value |> uint32
+
+        override self.FeeProportionalMillionths =
+            self.Update.FeeProportionalMillionths
+
+        override self.CltvExpiryDelta =
+            self.Update.CLTVExpiryDelta.Value |> uint32
+
         override self.HTLCMaximumMSat = self.Update.HTLCMaximumMSat
         override self.HTLCMinimumMSat = self.Update.HTLCMinimumMSat
 
@@ -87,13 +93,20 @@ module RoutingHeuristics =
 
 
 module EdgeWeightCaluculation =
-    let nodeFee (baseFee: LNMoney) (proportionalFee: int64) (paymentAmount: LNMoney) =
-        baseFee + LNMoney.Satoshis(decimal(paymentAmount.Satoshi * proportionalFee) / 1000000.0m)
-        
+    let nodeFee
+        (baseFee: LNMoney)
+        (proportionalFee: int64)
+        (paymentAmount: LNMoney)
+        =
+        baseFee
+        + LNMoney.Satoshis(
+            decimal(paymentAmount.Satoshi * proportionalFee) / 1000000.0m
+        )
+
     let edgeFeeCost (amountWithFees: LNMoney) (edge: IRoutingHopInfo) =
         let result =
             nodeFee
-                edge.FeeBaseMSat 
+                edge.FeeBaseMSat
                 (int64 edge.FeeProportionalMillionths)
                 amountWithFees
         // We can't have zero fee cost because it causes weight to be 0 regardless of expiry_delta
@@ -103,27 +116,35 @@ module EdgeWeightCaluculation =
     let edgeWeight (paymentAmount: LNMoney) (edge: IRoutingHopInfo) : float =
         let feeCost = float (edgeFeeCost paymentAmount edge).Value
         let channelCLTVDelta = edge.CltvExpiryDelta
+
         let edgeMaxCapacity =
             edge.HTLCMaximumMSat
             |> Option.defaultValue(RoutingHeuristics.CAPACITY_CHANNEL_LOW)
+
         if edgeMaxCapacity < paymentAmount then
             infinity // chanel capacity is too small, reject edge
         elif paymentAmount < edge.HTLCMinimumMSat then
             infinity // our payment is too small for the channel, reject edge
         else
             let capFactor =
-                1.0 - RoutingHeuristics.normalize(
-                        float edgeMaxCapacity.MilliSatoshi,
-                        float RoutingHeuristics.CAPACITY_CHANNEL_LOW.MilliSatoshi,
-                        float RoutingHeuristics.CAPACITY_CHANNEL_HIGH.MilliSatoshi)
+                1.0
+                - RoutingHeuristics.normalize(
+                    float edgeMaxCapacity.MilliSatoshi,
+                    float RoutingHeuristics.CAPACITY_CHANNEL_LOW.MilliSatoshi,
+                    float RoutingHeuristics.CAPACITY_CHANNEL_HIGH.MilliSatoshi
+                )
+
             let cltvFactor =
                 RoutingHeuristics.normalize(
                     float channelCLTVDelta,
                     float RoutingHeuristics.CLTV_LOW,
-                    float RoutingHeuristics.CLTV_HIGH)
-            let factor = 
-                cltvFactor * RoutingHeuristics.CltvDeltaFactor 
+                    float RoutingHeuristics.CLTV_HIGH
+                )
+
+            let factor =
+                cltvFactor * RoutingHeuristics.CltvDeltaFactor
                 + capFactor * RoutingHeuristics.CapacityFactor
+
             factor * feeCost
 
 
@@ -132,18 +153,30 @@ type ChannelUpdates =
         Forward: UnsignedChannelUpdateMsg option
         Backward: UnsignedChannelUpdateMsg option
     }
-    static member Empty = { Forward = None; Backward = None }
-    
+
+    static member Empty =
+        {
+            Forward = None
+            Backward = None
+        }
+
     member self.With(update: UnsignedChannelUpdateMsg) =
         let isForward = (update.ChannelFlags &&& 1uy) = 0uy
+
         if isForward then
             match self.Forward with
             | Some(prevUpd) when update.Timestamp < prevUpd.Timestamp -> self
-            | _ -> { self with Forward = Some(update) }
+            | _ ->
+                { self with
+                    Forward = Some(update)
+                }
         else
             match self.Backward with
             | Some(prevUpd) when update.Timestamp < prevUpd.Timestamp -> self
-            | _ -> { self with Backward = Some(update) }
+            | _ ->
+                { self with
+                    Backward = Some(update)
+                }
 
     member self.Combine(other: ChannelUpdates) =
         let combine upd1opt upd2opt : UnsignedChannelUpdateMsg option =
@@ -151,64 +184,117 @@ type ChannelUpdates =
             | None, None -> None
             | Some(_), None -> upd1opt
             | None, Some(_) -> upd2opt
-            | Some(upd1), Some(upd2) -> if upd1.Timestamp > upd2.Timestamp then upd1opt else upd2opt
-        { Forward = combine self.Forward other.Forward; Backward = combine self.Backward other.Backward }
+            | Some(upd1), Some(upd2) ->
+                if upd1.Timestamp > upd2.Timestamp then
+                    upd1opt
+                else
+                    upd2opt
+
+        {
+            Forward = combine self.Forward other.Forward
+            Backward = combine self.Backward other.Backward
+        }
 
 
-type RoutingGraphData private(announcements: Set<UnsignedChannelAnnouncementMsg>, 
-                                updates: Map<ShortChannelId, ChannelUpdates>,
-                                lastSyncTimestamp: uint32,
-                                blacklistedChannels: Set<ShortChannelId>,
-                                routingGraph: RoutingGraph) =
-        
-    new() = RoutingGraphData(Set.empty, Map.empty, 0u, Set.empty, RoutingGraph(AdjacencyGraph()))
+type RoutingGraphData
+    private
+    (
+        announcements: Set<UnsignedChannelAnnouncementMsg>,
+        updates: Map<ShortChannelId, ChannelUpdates>,
+        lastSyncTimestamp: uint32,
+        blacklistedChannels: Set<ShortChannelId>,
+        routingGraph: RoutingGraph
+    ) =
+
+    new() =
+        RoutingGraphData(
+            Set.empty,
+            Map.empty,
+            0u,
+            Set.empty,
+            RoutingGraph(AdjacencyGraph())
+        )
 
     member self.LastSyncTimestamp = lastSyncTimestamp
 
     member self.Graph = routingGraph
 
-    member self.Update (newAnnouncements : seq<UnsignedChannelAnnouncementMsg>) 
-                        (newUpdates: Map<ShortChannelId, ChannelUpdates>) 
-                        (syncTimestamp: uint32) : RoutingGraphData =
-        let announcements = 
-            announcements 
-            |> Set.union (newAnnouncements |> Set.ofSeq)
-            |> Set.filter (fun ann -> not (blacklistedChannels |> Set.contains ann.ShortChannelId))
-            
+    member self.Update
+        (newAnnouncements: seq<UnsignedChannelAnnouncementMsg>)
+        (newUpdates: Map<ShortChannelId, ChannelUpdates>)
+        (syncTimestamp: uint32)
+        : RoutingGraphData =
+        let announcements =
+            announcements
+            |> Set.union(newAnnouncements |> Set.ofSeq)
+            |> Set.filter(fun ann ->
+                not(blacklistedChannels |> Set.contains ann.ShortChannelId)
+            )
+
         let updates =
             if updates.IsEmpty then
                 newUpdates
             else
                 let mutable tmpUpdates = updates
-                newUpdates |> Map.iter (fun channelId newUpd ->
+
+                newUpdates
+                |> Map.iter(fun channelId newUpd ->
                     match tmpUpdates |> Map.tryFind channelId with
                     | Some upd ->
-                        tmpUpdates <- tmpUpdates |> Map.add channelId (upd.Combine newUpd)
+                        tmpUpdates <-
+                            tmpUpdates |> Map.add channelId (upd.Combine newUpd)
                     | None ->
-                        tmpUpdates <- tmpUpdates |> Map.add channelId newUpd )
+                        tmpUpdates <- tmpUpdates |> Map.add channelId newUpd
+                )
+
                 tmpUpdates
 
         let baseGraph = AdjacencyGraph<NodeId, RoutingGrpahEdge>()
 
         for ann in announcements do
             let updates = updates.[ann.ShortChannelId]
-                    
-            let addEdge source target (upd : UnsignedChannelUpdateMsg) =
-                let edge = { Source = source; Target = target; ShortChannelId = upd.ShortChannelId; Update = upd }
-                baseGraph.AddVerticesAndEdge edge |> ignore
-                
-            updates.Forward |> Option.iter (addEdge ann.NodeId1 ann.NodeId2)
-            updates.Backward |> Option.iter (addEdge ann.NodeId2 ann.NodeId1)
 
-        RoutingGraphData(announcements, updates, syncTimestamp, blacklistedChannels, RoutingGraph(baseGraph))
+            let addEdge source target (upd: UnsignedChannelUpdateMsg) =
+                let edge =
+                    {
+                        Source = source
+                        Target = target
+                        ShortChannelId = upd.ShortChannelId
+                        Update = upd
+                    }
+
+                baseGraph.AddVerticesAndEdge edge |> ignore
+
+            updates.Forward |> Option.iter(addEdge ann.NodeId1 ann.NodeId2)
+            updates.Backward |> Option.iter(addEdge ann.NodeId2 ann.NodeId1)
+
+        RoutingGraphData(
+            announcements,
+            updates,
+            syncTimestamp,
+            blacklistedChannels,
+            RoutingGraph(baseGraph)
+        )
 
     member self.BlacklistChannel(shortChannelId: ShortChannelId) =
-        let newBlacklistedChannels = blacklistedChannels |> Set.add shortChannelId
+        let newBlacklistedChannels =
+            blacklistedChannels |> Set.add shortChannelId
+
         let baseGraph = AdjacencyGraph<NodeId, RoutingGrpahEdge>()
+
         baseGraph.AddVerticesAndEdgeRange(
-            self.Graph.Edges |> Seq.filter (fun edge ->  edge.ShortChannelId <> shortChannelId))
-            |> ignore
-        RoutingGraphData(announcements, updates, self.LastSyncTimestamp, newBlacklistedChannels, RoutingGraph(baseGraph))
+            self.Graph.Edges
+            |> Seq.filter(fun edge -> edge.ShortChannelId <> shortChannelId)
+        )
+        |> ignore
+
+        RoutingGraphData(
+            announcements,
+            updates,
+            self.LastSyncTimestamp,
+            newBlacklistedChannels,
+            RoutingGraph(baseGraph)
+        )
 
     member self.GetChannelUpdates() =
         updates
@@ -217,11 +303,19 @@ type RoutingGraphData private(announcements: Set<UnsignedChannelAnnouncementMsg>
     /// Don't use channels that have insufficient capacity for given paymentAmount.
     /// See EdgeWeightCaluculation.edgeWeight.
     /// If no routes can be found, return empty sequence.
-    member self.GetRoute (sourceNodeId: NodeId) (targetNodeId: NodeId) (paymentAmount: LNMoney) : seq<RoutingGrpahEdge> =
-        let tryGetPath = 
+    member self.GetRoute
+        (sourceNodeId: NodeId)
+        (targetNodeId: NodeId)
+        (paymentAmount: LNMoney)
+        : seq<RoutingGrpahEdge> =
+        let tryGetPath =
             routingGraph.ShortestPathsDijkstra(
-                System.Func<RoutingGrpahEdge, float>(EdgeWeightCaluculation.edgeWeight paymentAmount), 
-                sourceNodeId)
+                System.Func<RoutingGrpahEdge, float>(
+                    EdgeWeightCaluculation.edgeWeight paymentAmount
+                ),
+                sourceNodeId
+            )
+
         match tryGetPath.Invoke targetNodeId with
         | true, path -> path
         | false, _ -> Seq.empty
